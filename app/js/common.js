71b2eb970922e247a6baf09c77d90fdf70ea4f0d
YakovSPb <prospektpro673@gmail.com>
2019-02-28 16:34:52 +0400

    Site is ready

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,47 @@
 $(function() {
+ $(window).on('load resize', function() {
+        var $width = $(window).width();
+        
+            var WidthScreen = 1170+($width-1170)/2;
+           	if (screen.width > 1200){
+             	$('header').css('background-size', WidthScreen);
+             }
+    });
 
-	// Custom JS
 
+// 	if (screen.width > 1200){
+// 	var WidthScreen = 1170+(screen.width-1170)/2;
+
+// 	alert(WidthScreen);
+
+// 	$('header').css('background-size', WidthScreen);
+// }
+
+
+	if (screen.width < 992){
+			$('.block2 br').remove();
+	}
+
+
+     $("form").submit(function() { //Change
+            var th = $(this);
+            $.ajax({
+                type: "POST",
+                url: "mail.php", //Change
+                data: th.serialize()
+            }).done(function() {
+                $(".success").addClass("visible");
+                setTimeout(function() {
+                    // Done Functions
+                    th.trigger("reset");
+
+                $(".success").removeClass("visible");
+                $.magnificPopup.close();
+                }, 3000);
+            });
+            return false;
+        });
+
+	
 });
+
